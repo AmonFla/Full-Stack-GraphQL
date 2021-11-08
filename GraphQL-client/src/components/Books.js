267f7d445319798ed222704804c5bd21f5d48a55
Flavio Garrido <fbonnin@teceng-gaming.com.ar>
2021-11-08 11:40:36 -0300

    Ex8.19

--- GraphQL-client/src/components/Books.js
+++ GraphQL-client/src/components/Books.js
@@ -1,21 +1,32 @@
 import { useQuery } from '@apollo/client'
 import React, { useEffect, useState } from 'react'
 import { BOOKS_GET_ALL } from '../graphql/books'
+import { union, includes } from 'lodash'
 
 
 const Books = (props) => {
   const [books, setBooks] = useState([])
   const result = useQuery(BOOKS_GET_ALL)
+  const [filter, setFilter] = useState('')
+  const [genres, setGenres] = useState([])
   
   useEffect(()=>{
     if(result.data){
       setBooks(result.data.allBooks)
+      setGenres(result.data.allBooks.reduce((genres, book) => union(genres,book.genres), [])) 
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   },[result])
 
   if (!props.show) {
     return null
   }
+
+  let bookList = books
+  if(filter !== '')
+    bookList = books.filter(b => includes(b.genres, filter) )
+  
+  
 
   return (
     <div>
@@ -32,7 +43,7 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {books.map(a =>
+          {bookList.map(a =>
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -41,6 +52,7 @@ const Books = (props) => {
           )}
         </tbody>
       </table>
+      {genres.map(genre=> <button onClick={()=> setFilter(genre)}>{genre}</button> )}<button onClick={()=> setFilter('')}>Todos</button>
     </div>
   )
 }
